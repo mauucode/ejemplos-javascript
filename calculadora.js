4f5e623895a67131c95038b9759a8f865edb3041
Mauricio Núñez Pulido <mauricionpulido@gmail.com>
2026-01-20 15:09:59 -0600

    cambios calculadora e index

--- calculadora.js
+++ calculadora.js
@@ -1,1 +1,20 @@
-alert("Hola desde calculadora.js");
+// alert("Hola desde calculadora.js");
+
+function suma() {
+
+    let numero1 = parseInt(document.getElementById("numero1").value);
+    let numero2 = parseInt(document.getElementById("numero2").value);
+
+    let resultado = 0;
+    resultado = numero1 + numero2;
+
+    let objResultado = document.getElementById("resultado");
+    objResultado.innerHTML = resultado;
+
+
+
+
+
+
+
+}
