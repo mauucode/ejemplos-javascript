dc972165ac37e3174aa7b7b3e23943a9fb986447
Mauricio Núñez Pulido <mauricionpulido@gmail.com>
2026-01-21 14:27:05 -0600

    calculadora terminada

--- calculadora.js
+++ calculadora.js
@@ -1,20 +1,33 @@
 // alert("Hola desde calculadora.js");
 
-function suma() {
+function mostrar(e) {
 
     let numero1 = parseInt(document.getElementById("numero1").value);
     let numero2 = parseInt(document.getElementById("numero2").value);
-
     let resultado = 0;
-    resultado = numero1 + numero2;
+    let operacion = e.target.id;
 
-    let objResultado = document.getElementById("resultado");
-    objResultado.innerHTML = resultado;
+    if (operacion === "suma"){
+        resultado= numero1 + numero2;
+    }
+    else if (operacion === "resta"){
+        resultado= numero1 - numero2;
+    }
+    else if (operacion === "multiplica"){
+        resultado= numero1 * numero2;
+    }
+    else if (operacion === "divide"){
+        resultado= numero1 / numero2;
+    }
+    else {
+        alert ("Funcionalidad en progreso")
+    }
 
-
-
-
-
-
+   
+     let result =  document.getElementById("resultado");
+     result.innerHTML=resultado;
 
 }
+
+let butones = document.getElementById("btns");
+butones.addEventListener("click", mostrar);
